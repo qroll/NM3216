dcf0300a6b1562469571441115259a40e3900e9b
roll <quek.ruoling@gmail.com>
2017-10-18 17:45:01 +0800

    Refactor
    - Rename Movement script to Enemy
    - More sensible variable names
    - Add tooltips for some fields in the inspector
    - Extract function to reset game status

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+    public bool isTrapped = false;
+    public float movement = 1;
+
+    public float angle = 0;
+    public float radius = 0.5f;
+
+    public int hitsLeft = 1;
+
+    // Use this for initialization
+    void Start()
+    {
+
+	}
+	
+	// Update is called once per frame
+	void Update()
+    {
+        if (isTrapped)
+        {
+            float x = radius * Mathf.Sin(angle);
+            float y = radius * Mathf.Cos(angle);
+            Vector3 position = new Vector3(x, y, transform.position.z);
+
+            // update position and rotation around the pivot point
+            transform.position = position;
+            transform.right = position;
+
+            angle += Time.deltaTime;
+        } else
+        {
+            transform.Translate(movement * new Vector3(0, -1, 0) * Time.deltaTime);
+        }
+	}
+
+    // Returns true if the enemy is killed on this hit
+    public bool Swat()
+    {
+        hitsLeft--;
+        return hitsLeft <= 0;
+    }
+}
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,25 +4,43 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour {
 
-    public float distFromCamera = 10.0f;
-    public float startDelay = 1.0f;
-    public float swatRadius = 0.3f;
-    public float spawnFreqRate = 1.0f;
-    public float deltaSpawnFreqRate = 0.1f;
-    public float minSpawnFreqRate = 0.5f;
-    public float spawnNumRate = 1.0f;
-    public float deltaSpawnNumRate = 1.0f;
-    public float maxSpawnNumRate = 10.0f;
     public CDebug.EDebugLevel debugLevel = CDebug.EDebugLevel.TRACE;
 
+    public float distFromCamera = 10.0f;
+    [Tooltip("Time before enemies begin to spawn (seconds)")]
+    public float startDelay = 1.0f;
+    [Tooltip("Radius of swat zone")]
+    public float swatRadius = 1.0f;
+
+    // controls the frequency of spawning
+    [Tooltip("Starting frequency at which enemies are spawned")]
+    public float spawnFreqRate = 1.0f;
+    [Tooltip("Change in frequency on success")]
+    public float deltaSpawnFreqRate = 0.1f;
+    [Tooltip("Minumum frequency")]
+    public float minSpawnFreqRate = 0.5f;
+
+    // controls the number of enemies that are spawned
+    [Tooltip("Starting number of enemies spawned each time")]
+    public float spawnNumRate = 1.0f;
+    [Tooltip("Change in number of enemies on success")]
+    public float deltaSpawnNumRate = 0.5f;
+    [Tooltip("Maximum number of enemies")]
+    public float maxSpawnNumRate = 3.0f;
+    
     public Transform enemy;
+    public GameObject[] frogs;
+    public AudioSource audioSource;
+    public AudioClip whack;
+
+    // sprites
     public Sprite frogDisabledSprite;
     public Sprite frogEnabledSprite;
-    public GameObject[] swatters;
-    public GameObject[] frogs;
-    public GameObject menuCanvas;
-    public GameObject inGameCanvas;
-    public GameObject endGameCanvas;
+
+    // UI elements
+    public GameObject inGameUI;
+    public GameObject pauseGameUI;
+    public GameObject endGameUI;
 
     public const string ZONE_UP = "Up";
     public const string ZONE_DOWN = "Down";
@@ -32,50 +50,44 @@ public class GameManager : MonoBehaviour {
     private static string[] ZONE_AXES = { "Vertical", "Horizontal" };
     private static float AXIS_MIN = 0.3f;
     private static float AXIS_MAX = 0.7f;
-    private static float STUN_TIME = 2.0f;
+    private static float STUN_TIME = 1.0f;
+    private static int MAX_NUM_ENEMIES = 5;
 
     // Game status
     private bool isGameOver = false;
     private int killCount = 0;
     private float lastIncreased = 0;
+    private float currSpawnFreqRate = 1.0f;
+    private float currSpawnNumRate = 1.0f;
     private Queue<GameObject> infestationZone = new Queue<GameObject>();
-    private Dictionary<string, bool> swatterEnabled = new Dictionary<string, bool>();
-    private Dictionary<string, GameObject> swatterSprites = new Dictionary<string, GameObject>();
+    private Dictionary<string, bool> isFrogEnabledInZone = new Dictionary<string, bool>();
+    private Dictionary<string, GameObject> frogInZone = new Dictionary<string, GameObject>();
 
     // Use this for initialization
-    void Start () {
+    void Start()
+    {
         CDebug.SetDebugLoggingLevel((int) debugLevel);
         
-        // initialise swatters
-        foreach (GameObject swatter in swatters)
-        {
-            swatter.SetActive(false);
-        }
+        frogInZone.Add("Up", frogs[0]);
+        frogInZone.Add("Down", frogs[1]);
+        frogInZone.Add("Left", frogs[2]);
+        frogInZone.Add("Right", frogs[3]);
 
-        swatterEnabled.Add("Up", true);
-        swatterEnabled.Add("Down", true);
-        swatterEnabled.Add("Left", true);
-        swatterEnabled.Add("Right", true);
-
-        swatterSprites.Add("Up", frogs[0]);
-        swatterSprites.Add("Down", frogs[1]);
-        swatterSprites.Add("Left", frogs[2]);
-        swatterSprites.Add("Right", frogs[3]);
-
-        menuCanvas.SetActive(false);
+        ResetGame();
     }
 	
 	// Update is called once per frame
-	void Update () {
+	void Update()
+    {
         if (isGameOver)
         {
             return;
         }
 
-        CDebug.Log(CDebug.EDebugLevel.DEBUG, string.Format("time={0} | last update={1}", Time.time, (startDelay + lastIncreased)));
-        if (Time.time - (startDelay + lastIncreased) > spawnFreqRate)
+        // CDebug.Log(CDebug.EDebugLevel.DEBUG, string.Format("time={0} | last update={1}", Time.time, (startDelay + lastIncreased)));
+        if (Time.time - (startDelay + lastIncreased) > currSpawnFreqRate)
         {
-            for (int i = 0; i < spawnNumRate; i++)
+            for (int i = 0; i < currSpawnNumRate; i++)
             {
                 SpawnEnemy();
             }
@@ -89,14 +101,13 @@ public class GameManager : MonoBehaviour {
         string zone = GenerateZone();
         Vector3 position = GeneratePosition(zone);
         Quaternion rotation = GenerateRotation(position);
-        CDebug.Log(CDebug.EDebugLevel.TRACE, string.Format("spawn position={0} | rotation={1} | time={2}", position, rotation, Time.time));
+
+        CDebug.Log(CDebug.EDebugLevel.DEBUG, string.Format("spawn position={0} | rotation={1} | time={2}", position, rotation, Time.time));
+
         GameObject spawn = Instantiate(enemy, position, rotation).gameObject;
         GameObject go = new GameObject("Zone");
         go.tag = "Zone" + zone;
         go.transform.parent = spawn.transform;
-
-        Movement control = (Movement) spawn.GetComponent("Movement");
-        control.zone = zone;
     }
 
     // Returns a random zone: Up, Down, Left or Right
@@ -138,7 +149,7 @@ public class GameManager : MonoBehaviour {
     // Returns the rotation needed for a game object to face the origin
     Quaternion GenerateRotation(Vector3 position)
     {
-        Vector3 direction = new Vector3(0,0,0) - position;
+        Vector3 direction = -position;
         float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg + 90;
         Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.forward);
 
@@ -154,27 +165,31 @@ public class GameManager : MonoBehaviour {
         // prioritize enemies in the infestation zone
         if (infestationZone.Count > 0)
         {
-            CDebug.Log(CDebug.EDebugLevel.TRACE, "destroying bug in infestation zone");
             GameObject enemy = infestationZone.Dequeue();
-            AnimateFrog(zone, enemy, true);
+            AnimateFrogTongue(zone, enemy, true);
             Object.Destroy(enemy);
             return;
         }
 
         GameObject closest = FindClosestEnemy(zone);
-        bool isSwatterActive = swatterEnabled[zone];
+        bool isSwatterActive = isFrogEnabledInZone[zone];
 
         if (isSwatterActive && closest != null)
         {
             // if swatter is enabled and at least one enemy is within the swat zone,
             // destroy the closest enemy
-            AnimateFrog(zone, closest, false);
-            Object.Destroy(closest.transform.parent.gameObject);
+            AnimateFrogTongue(zone, closest, false);
+            audioSource.PlayOneShot(whack);
+            if (closest.GetComponent<Enemy>().Swat())
+            {
+                Object.Destroy(closest);
+                EnemySwatted();
+            }
         } else if (isSwatterActive)
         {
             // no enemy was found within the swat zone, disable the swatter temporarily
-            swatterEnabled[zone] = false;
-            SpriteRenderer sr = swatterSprites[zone].GetComponent<SpriteRenderer>();
+            isFrogEnabledInZone[zone] = false;
+            SpriteRenderer sr = frogInZone[zone].GetComponent<SpriteRenderer>();
             sr.sprite = frogDisabledSprite;
             StartCoroutine(EnableSwatter(STUN_TIME, zone));
         }
@@ -203,7 +218,7 @@ public class GameManager : MonoBehaviour {
             }
         }
 
-        return closest;
+        return closest == null ? closest : closest.transform.parent.gameObject;
     }
 
     IEnumerator DisableSwatter(float delay, GameObject obj)
@@ -215,24 +230,24 @@ public class GameManager : MonoBehaviour {
     IEnumerator EnableSwatter(float delay, string zone)
     {
         yield return new WaitForSeconds(delay);
-        swatterEnabled[zone] = true;
-        SpriteRenderer sr = swatterSprites[zone].GetComponent<SpriteRenderer>();
+        isFrogEnabledInZone[zone] = true;
+        SpriteRenderer sr = frogInZone[zone].GetComponent<SpriteRenderer>();
         sr.sprite = frogEnabledSprite;
     }
 
-    // Animates the frog swatting
+    // Animates the frog
     // The tongue extends towards the specified enemy and retracts afterwards
     // If turn bool is true, the frog will turn around first
-    private void AnimateFrog(string zone, GameObject obj, bool turn)
+    private void AnimateFrogTongue(string zone, GameObject enemy, bool turn)
     {
-        GameObject frog = swatterSprites[zone];
+        GameObject frog = frogInZone[zone];
         Transform tongue = frog.transform.Find("tongue");
 
         Vector3 originalScale = tongue.localScale;
         Vector3 originalPosition = tongue.position;
         Quaternion originalRotation = tongue.rotation;
         
-        ExtendTongue(frog, obj, turn);
+        ExtendTongue(frog, enemy, turn);
         StartCoroutine(RetractTongue(frog, originalScale, originalPosition, originalRotation, turn));
     }
     
@@ -248,9 +263,9 @@ public class GameManager : MonoBehaviour {
         Transform tongue = frog.transform.Find("tongue");
 
         Vector3 direction = enemy.transform.position - tongue.position;
+        float targetSize = direction.magnitude;
         Vector3 localScale = tongue.localScale;
         
-        float targetSize = direction.magnitude;
         float currentSize = tongue.Find("tongue-length").GetComponent<Renderer>().bounds.size.y;
         float scale = localScale.y * (targetSize / currentSize);
 
@@ -266,7 +281,7 @@ public class GameManager : MonoBehaviour {
         Transform tongue = frog.transform.Find("tongue");
 
         // reset the sprite
-        tongue.localScale = originalScale;
+        tongue.localScale = new Vector3(1, 0.1f, 1);
         tongue.rotation = originalRotation;
         if (turn)
         {
@@ -279,23 +294,25 @@ public class GameManager : MonoBehaviour {
     public void EnemySwatted()
     {
         killCount++;
-        spawnFreqRate = Mathf.Max(spawnFreqRate - deltaSpawnFreqRate, minSpawnFreqRate);
-        spawnNumRate = Mathf.Min(spawnNumRate + deltaSpawnNumRate, maxSpawnNumRate);
+        currSpawnFreqRate = Mathf.Max(currSpawnFreqRate - deltaSpawnFreqRate, minSpawnFreqRate);
+        currSpawnNumRate = Mathf.Min(currSpawnNumRate + deltaSpawnNumRate, maxSpawnNumRate);
         CDebug.Log(CDebug.EDebugLevel.INFO, "kill count=" + killCount + " | spawn freq=" + spawnFreqRate + " | spawn num=" + spawnNumRate);
     }
 
     // Inform GameManager that an enemy has reached the infestation zone
     public void EnemyReached(GameObject obj)
     {
-        Movement control = (Movement) obj.GetComponent("Movement");
+        Enemy control = (Enemy) obj.GetComponent("Enemy");
+        control.angle = Mathf.Atan2(obj.transform.position.y, obj.transform.position.x);
         control.isTrapped = true;
         infestationZone.Enqueue(obj);
 
-        if (infestationZone.Count >= 3)
+        if (infestationZone.Count >= MAX_NUM_ENEMIES)
         {
             CDebug.Log(CDebug.EDebugLevel.INFO, "game over");
             isGameOver = true;
-            endGameCanvas.SetActive(true);
+            inGameUI.SetActive(false);
+            endGameUI.SetActive(true);
         }
     }
 
@@ -303,30 +320,38 @@ public class GameManager : MonoBehaviour {
     {
         Time.timeScale = 0;
 
-        menuCanvas.SetActive(true);
+        pauseGameUI.SetActive(true);
+        inGameUI.SetActive(false);
     }
 
     public void OnResumeButton()
     {
         Time.timeScale = 1.0f;
 
-        menuCanvas.SetActive(false);
+        pauseGameUI.SetActive(false);
+        inGameUI.SetActive(true);
     }
 
     public void OnPlayButton()
     {
-        menuCanvas.SetActive(false);
-        endGameCanvas.SetActive(false);
-        
-        // enable all swatters
-        swatterEnabled.Clear();
-        swatterEnabled.Add("Up", true);
-        swatterEnabled.Add("Down", true);
-        swatterEnabled.Add("Left", true);
-        swatterEnabled.Add("Right", true);
+        ResetGame();
+    }
+
+    void ResetGame()
+    {
+        inGameUI.SetActive(true);
+        pauseGameUI.SetActive(false);
+        endGameUI.SetActive(false);
+
+        // enable all frogs
+        isFrogEnabledInZone.Clear();
+        isFrogEnabledInZone.Add("Up", true);
+        isFrogEnabledInZone.Add("Down", true);
+        isFrogEnabledInZone.Add("Left", true);
+        isFrogEnabledInZone.Add("Right", true);
 
         // reset frog sprites to normal state
-        foreach (GameObject sprite in swatterSprites.Values)
+        foreach (GameObject sprite in frogInZone.Values)
         {
             SpriteRenderer sr = sprite.GetComponent<SpriteRenderer>();
             sr.sprite = frogEnabledSprite;
@@ -342,6 +367,8 @@ public class GameManager : MonoBehaviour {
         isGameOver = false;
         killCount = 0;
         lastIncreased = 0;
+        currSpawnFreqRate = spawnFreqRate;
+        currSpawnNumRate = spawnNumRate;
         infestationZone.Clear();
     }
 
