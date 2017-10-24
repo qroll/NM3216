779aa6e27f2ccb00b568d21b929bade365a45333
roll <quek.ruoling@gmail.com>
2017-10-24 17:28:47 +0800

    Add basic loading screen

--- Assets/Scripts/SceneController.cs
+++ Assets/Scripts/SceneController.cs
@@ -5,19 +5,33 @@ using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class SceneController : MonoBehaviour {
-    
+
+    public Text loadingText;
+
 	// Use this for initialization
 	void Start () {
-
+        StartCoroutine(LoadGame());
     }
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
-    
-    public void LoadGameOnClick()
+        loadingText.color = new Color(loadingText.color.r, loadingText.color.g, loadingText.color.b, Mathf.PingPong(Time.time, 1));
+    }
+
+    IEnumerator LoadGame()
     {
-        SceneManager.LoadScene("Game");
+        // yield return new WaitForSeconds(3);
+
+        AsyncOperation operation = SceneManager.LoadSceneAsync("Game", LoadSceneMode.Single);
+        operation.allowSceneActivation = false;
+
+        //Wait until the last operation fully loads to return anything
+        while (operation.progress < 0.9f)
+        {
+            loadingText.text = "Loading... " + string.Format("{0:P1}", operation.progress);
+            yield return null;
+        }
+
+        operation.allowSceneActivation = true;
     }
 }
