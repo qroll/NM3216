04ec55ad6eda5157a79d4a1d90daf8edb83562d5
roll <quek.ruoling@gmail.com>
2017-11-15 22:48:16 +0800

    Add team name and urls to credits

---
+++ Assets/Scripts/HyperlinkWizard.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HyperlinkWizard : MonoBehaviour {
+    
+    public void OpenUrl(string url)
+    {
+        Application.OpenURL(url);
+    }
+
+}
